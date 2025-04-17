9473730e173f71c64a1bd12bea6ae0f7f0971efa
Guojf <guo123fighting@163.com>
2025-04-18 00:42:35 +0800

    add pybind and python

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# qttest
-qttest
+# Perception
+Perception
---
+++ pybind/pybinder.cpp
@@ -0,0 +1,33 @@
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+
+namespace py = pybind11;
+
+class Calculator {
+public:
+    int add(int a, int b) {
+        return a + b;
+    }
+
+    int subtract(int a, int b) {
+        return a - b;
+    }
+
+    int multiply(int a, int b) {
+        return a * b;
+    }
+
+    int divide(int a, int b) {
+        if (b == 0) throw std::invalid_argument("Division by zero");
+        return a / b;
+    }
+};
+
+PYBIND11_MODULE(calculator, m) {
+    py::class_<Calculator>(m, "Calculator")
+        .def(py::init<>())
+        .def("add", &Calculator::add)
+        .def("subtract", &Calculator::subtract)
+        .def("multiply", &Calculator::multiply)
+        .def("divide", &Calculator::divide);
+}
